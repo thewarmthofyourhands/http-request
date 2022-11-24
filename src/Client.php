a6b08b69c3448f2ed6fce7f4d4c15829aeacb0c4
thewarmthofyourhands <rostislavpanteleychuk@gmail.com>
2022-11-25 02:29:35 +0400

    fix

--- src/Client.php
+++ src/Client.php
@@ -17,6 +17,6 @@ class Client implements ClientInterface
 
     final public function sendRequest(RequestInterface $request): ResponseInterface
     {
-        return $this->requestSender->sendSocket($request);
+        return $this->requestSender->send($request);
     }
 }
